b742233d9ab84520e5d49e3375e94da76a6246e7
ddamiankowalski <dkowalski.1997@gmail.com>
2022-01-26 01:04:27 +0100

    komentarz

--- opencv_test/main.cpp
+++ opencv_test/main.cpp
@@ -1,4 +1,7 @@
 #include "opencv2/opencv.hpp"
+#include <vector>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
 
@@ -18,6 +21,7 @@ for(;;)
     Mat frame;
     cap >> frame;    
     imshow("Video", frame);
+    // imwrite("alpha.png", frame); ta linia zapisuje image w folderze, moze byc potrzebne w przyszlosci
     if(waitKey(30) >= 0) break;
 }
 
