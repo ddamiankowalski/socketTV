eaa8c63bc1bc31295f4a1ba709456288c19bd218
ddamiankowalski <dkowalski.1997@gmail.com>
2022-01-27 02:19:10 +0100

    efekt koncowy

--- socket_test/client.cpp
+++ socket_test/client.cpp
@@ -7,15 +7,44 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include "opencv2/opencv.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-#define PORT 8080
+#define PORT 8081
+
+using namespace cv;
 
 void error(const char *msg);
 int create_connection();
+int download_frame(int servsockfd);
+void send_confirmation(char const *msg, int socketfd);
+Mat receive_frame(int sockfd);
 
 int main(int, char**)
 {
+    int valread;
     int socketfd = create_connection();
+    char buffer[1024] = {0};
+
+    Mat img = imread("sample.jpeg", IMREAD_COLOR);
+    namedWindow("Client",1);
+
+    for(;;)
+    {
+        imshow("Client", img);
+
+        if(waitKey(30) >= 0) break;
+
+        // wysylamy informacje ze jestesmy gotowi na kolejna ramke
+        send_confirmation("ready", socketfd);
+
+        // czytamy kolejna ramke tutaj
+        img = receive_frame(socketfd);
+
+        // zapisujemy ramke do mat i wyswietlamy
+    }
 
     return 0;
 }
@@ -55,4 +84,33 @@ int create_connection()
     send(sockfd, msg, strlen(msg), 0);
 
     return sockfd;
+}
+
+void send_confirmation(char const *msg, int socketfd)
+{
+    send(socketfd, msg, strlen(msg), 0);
+}
+
+Mat receive_frame(int sockfd)
+{
+    Mat img = Mat::zeros(360, 640, CV_8UC3);
+    int imgSize = img.total() * img.elemSize();
+    uchar sockData[imgSize];
+    int bytes = 0;
+
+    for (int i = 0; i < imgSize; i += bytes) {
+        if ((bytes = recv(sockfd, sockData +i, imgSize  - i, 0)) == -1)  {
+            error("FRAME SENDING FAILED");
+        }
+    }
+
+    int ptr=0;
+    for (int i = 0;  i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {                                     
+            img.at<cv::Vec3b>(i,j) = cv::Vec3b(sockData[ptr+ 0],sockData[ptr+1],sockData[ptr+2]);
+            ptr=ptr+3;
+        }
+    }
+
+    return img;
 }
--- socket_test/server.cpp
+++ socket_test/server.cpp
@@ -5,23 +5,60 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <opencv2/opencv.hpp>
 
-#define SERVER_PORT 8080
+#define SERVER_PORT 8081
+
+using namespace cv;
 
 void error(const char *msg);
 int setup_server(int server_port);
 int accept_new_connection(int server_socket);
-
+void send_current_frame(Mat frame, int sockfd);
 
 int main(int, char**)
 {
     int server_socket = setup_server(SERVER_PORT);
 
+    fd_set current_sockets, ready_sockets;
+
+    FD_ZERO(&current_sockets);
+    FD_SET(server_socket, &current_sockets);
+
+    VideoCapture capture;
+    capture.open("sample.mp4");
+    namedWindow("Server", 1);
+    Mat frame;
+
     while(true)
     {
-        printf("WAITING FOR CONNECTIONS");
+        capture >> frame;
+        imshow("Server", frame);
+        if(waitKey(30) >= 0) break;
 
-        int client_socket = accept_new_connection(server_socket);
+        ready_sockets = current_sockets;
+
+        if(select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
+        {
+            error("ERROR WHILE SELECT");
+        }
+
+        for(int i = 0; i < FD_SETSIZE; i++)
+        {
+            if(FD_ISSET(i, &ready_sockets))
+            {
+                if(i == server_socket)
+                {
+                    // to oznacza ze server_socket moze przyjac kolejne polaczenie, wiec akceptujemy kolejne polaczenie
+                    int client_socket = accept_new_connection(server_socket);
+                    FD_SET(client_socket, &current_sockets);
+                } else
+                {
+                    // handle the connection, robimy tutaj cokolwiek co chcemy zrobic z socketem
+                    send_current_frame(frame, i);
+                }
+            }
+        }
     }
 
     return 0;
@@ -83,3 +120,10 @@ int accept_new_connection(int server_socket)
     return newsockfd;
 }
 
+void send_current_frame(Mat frame, int sockfd)
+{
+    int bytes;
+    int imgSize = frame.total() * frame.elemSize();
+
+    bytes = send(sockfd, frame.data, imgSize, 0);
+}
